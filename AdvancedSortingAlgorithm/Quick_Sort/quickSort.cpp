05c4e5c8786bfd9a3da2af6dad89b2420539e7c1
YatharthKumarSaxena <yatharthsaxena25@gmail.com>
2026-02-10 15:40:55 +0530

    Added Quick Sort Program

--- AdvancedSortingAlgorithm/Quick_Sort/quickSort.cpp
+++ AdvancedSortingAlgorithm/Quick_Sort/quickSort.cpp
@@ -1,47 +1,40 @@
-#include <iostream>
+#include <vector>
 using namespace std;
 
-int partition(int arr[],int startIdx,int endingIdx){
-    int pivotEle = arr[startIdx];
-    int count = 0;
-    for(int i=startIdx+1;i<=endingIdx;i++){
-        if(arr[i]<=pivotEle)count++;
+class Solution {
+  public:
+    void quickSort(vector<int>& arr, int low, int high) {
+        // code here
+        if(low>=high)return;
+        int pIdx = partition(arr,low,high);
+        quickSort(arr,low,pIdx-1);
+        quickSort(arr,pIdx+1,high);
     }
-    int pivotIdx = startIdx+count;
-    swap(arr[startIdx],arr[pivotIdx]);
-    int i = startIdx;
-    int j = endingIdx;
-    while(i<pivotIdx && j>pivotIdx){
-        if(arr[i]<=pivotEle)i++;
-        else if(arr[j]>pivotEle)j--;
-        else if(arr[i]>pivotEle && arr[j]<pivotEle){
-            swap(arr[i],arr[j]);
-            i++;
-            j--;
-        }
-    }
-    return pivotIdx;
-}
-void quickSort(int arr[],int startIdx,int endingIdx){
-    if(startIdx >= endingIdx)return;
-    int pivotIdx = partition(arr,startIdx,endingIdx);
-    quickSort(arr,startIdx,pivotIdx-1);
-    quickSort(arr,pivotIdx+1,endingIdx);
-}
 
-int main(){
-    cout<<"\nWelcome to the World of Programming\n";
-    int arr[] = {2,43,12,78,45,78,34,21,1,-5};
-    int n = sizeof(arr)/sizeof(int);
-    cout<<"Your Array looks as given below :-\n";
-    for(int i=0;i<n;i++){
-        cout<<arr[i]<<" ";
+  public:
+    int partition(vector<int>& arr, int low, int high) {
+        // code here
+        int pivotEle = arr[low];
+        int pIdx = low;
+        for(int i=low+1;i<=high;i++){
+            if(pivotEle>arr[i])pIdx++;
+        }
+        swap(arr[pIdx],arr[low]);
+        int i = low;
+        int j = high;
+        while(i < pIdx && j > pIdx){
+            if(arr[i] < pivotEle){
+                i++;
+            }
+            else if(arr[j] >= pivotEle){
+                j--;
+            }
+            else{
+                swap(arr[i], arr[j]);
+                i++;
+                j--;
+            }
+        }
+        return pIdx;
     }
-    cout<<endl;
-    quickSort(arr,0,n-1);
-    cout<<"After Quick Sort Array looks as given below:-\n";
-    for(int i=0;i<n;i++){
-        cout<<arr[i]<<" ";
-    }
-    return 0;
-}
+};
