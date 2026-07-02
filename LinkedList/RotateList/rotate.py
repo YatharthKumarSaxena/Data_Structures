52d7a0000bb796f1603a61f5874715bb1678e045
YatharthKumarSaxena <yatharthsaxena25@gmail.com>
2026-07-02 20:58:42 +0530

    Added More Programs Of Linked List

--- LinkedList/RotateList/rotate.py
+++ LinkedList/RotateList/rotate.py
@@ -1,10 +1,10 @@
 from typing import Optional
 
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 class Solution:
     def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
         if not head or not head.next:
